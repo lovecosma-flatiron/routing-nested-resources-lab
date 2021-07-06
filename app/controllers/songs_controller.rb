c30504ffb845081aa660254defd0f68b286c051f
Matthew Williams <matthew.williams@flatironschool.com>
2021-07-05 22:59:51 -0600

    commit tree cdc32e5a889f53ec2f58264f887ad92f16e97a2f

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,28 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    # binding.pry
+    if Artist.exists?(params[:artist_id])
+      @songs = Artist.find(params[:artist_id]).songs
+    elsif !params[:artist_id]
+      # @songs = Song.all
+        @songs = Song.all
+    else
+      flash[:alert] = "Artist not found"
+      redirect_to artists_path
+    end 
+
   end
 
   def show
-    @song = Song.find(params[:id])
+    if Song.exists?(params[:id])
+      @song = Song.find(params[:id])
+    elsif Song.exists?(params[:id]) && Artist.exists?(params[:artist_id])
+      @song = Song.find(params[:id])
+    elsif Song.exists?(params[:id]) && !Artist.exists?(params[:artist_id])
+      flash[:alert] = "Artist not found"
+      redirect_to artist_songs_path
+    elsif Song.exists?(params[:id]) && !Artist.exists?(params[:artist_id])
+    end 
   end
 
   def new
